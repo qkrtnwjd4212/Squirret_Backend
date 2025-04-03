d8c0c862eaf96f151f7e67e4038b8b408340f86d
jeon-jueun <dudwns0423@naver.com>
2025-04-04 00:23:48 +0900

    first commit

---
+++ src/main/java/com/squirret/squirretbackend/SquirretbackendApplication.java
@@ -0,0 +1,13 @@
+package com.squirret.squirretbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquirretbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquirretbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squirret/squirretbackend/SquirretbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirret.squirretbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquirretbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
